0fc4008e3835e7aaefee4064e7af4befde1daa0d
VadzimFiadoryn <55151630+VadzimFiadoryn@users.noreply.github.com>
2019-10-04 20:35:53 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var openBreket1 = '(';
+    var openBreket2 = '[';
+    var closeBreket1 = ')';
+    var closeBreket2 = ']';
+    var closOpenBreket3 = '|';
+    var closOpenBreket3 = "|"
+    var longBrekets = str.length;
+    var calcBreket1 = 0;
+    var calcBreket2 = 0;
+    var calcBreket3 = 0;
+    while (longBrekets >= 0) {
+        if (str[longBrekets] === closeBreket1) {
+            calcBreket1++;
+        }
+        if (str[longBrekets] === closeBreket2) {
+            calcBreket2++;
+        }
+        if (str[longBrekets] === openBreket1) {
+            if (calcBreket1 - 1 == 0 && calcBreket2 > 0) {
+                return false;
+            }
+            calcBreket1--;
+            if (calcBreket1 < 0) {
+                return false;
+            }
+        }
+        if (str[longBrekets] === openBreket2) {
+            calcBreket2--;
+            if (calcBreket2 < 0) {
+                return false;
+            }
+        }
+        if (str[longBrekets] === closOpenBreket3) {
+            calcBreket3++;
+        }
+        longBrekets--;
+    }
+    return (!(calcBreket1) && !(calcBreket1));
+}
 }
